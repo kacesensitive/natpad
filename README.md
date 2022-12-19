3aca2b7920d2be77ad052c3e67cfbb9a2c41dedd
Kc <kc@kacey.dev>
2022-12-18 23:53:48 -0600

    add readme + minor change to description

---
+++ README.md
@@ -0,0 +1,8 @@
+#NatPad
+
+This web-app is meant to help the D/deaf and hard of hearing
+community communicate with people who do not know ASL.
+Essentially a lightweight digital replacement for carrying a
+notepad.
+
+wip
--- src/App.jsx
+++ src/App.jsx
@@ -203,7 +203,7 @@ function App() {
               <Modal.Body>
                 <div className="text-center">
                   <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                    This web-app is meant to help the deaf and hard of hearing
+                    This web-app is meant to help the D/deaf and hard of hearing
                     community communicate with people who do not know ASL.
                     Essentially a lightweight digital replacement for carrying a
                     notepad.
